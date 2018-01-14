0df267bcdea7c0a5c0d362e789bd760532259c01
OTerrien <oterrien@neuf.fr>
2018-01-14 21:36:26 +0100

    handle other persistence exceptions

--- src/main/java/com/ote/crud/PersistenceExceptionHandler.java
+++ src/main/java/com/ote/crud/PersistenceExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.ote.crud;
 
 import com.ote.common.Error;
+import com.ote.crud.exception.CreateException;
+import com.ote.crud.exception.MergeException;
 import com.ote.crud.exception.NotFoundException;
+import com.ote.crud.exception.ResetException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +18,27 @@ public class PersistenceExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     Error handle(NotFoundException exception) {
+        log.error(exception.getMessage(), exception);
+        return new Error(exception.getMessage());
+    }
+
+    @ExceptionHandler(CreateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Error handle(CreateException exception) {
+        log.error(exception.getMessage(), exception);
+        return new Error(exception.getMessage());
+    }
+
+    @ExceptionHandler(MergeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Error handle(MergeException exception) {
+        log.error(exception.getMessage(), exception);
+        return new Error(exception.getMessage());
+    }
+
+    @ExceptionHandler(ResetException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    Error handle(ResetException exception) {
         log.error(exception.getMessage(), exception);
         return new Error(exception.getMessage());
     }
